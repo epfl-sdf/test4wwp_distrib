cc1663c9f216e5e2b21714509ca23dd47b4f776a
S-Rey <samuel.rey@epfl.ch>
2017-07-31 17:01:18 +0200

    Ajout d'un schéma relationnel et d'un générateur de tables

--- sql_create_tables.sql
+++ sql_create_tables.sql
@@ -1,51 +1,38 @@
--- Creator:       MySQL Workbench 6.3.9/ExportSQLite Plugin 0.1.0
--- Author:        Unknown
--- Caption:       New Model
--- Project:       Name of the project
--- Changed:       2017-07-31 15:08
--- Created:       2017-07-31 11:54
-
 BEGIN;
 CREATE TABLE browsers(
-  "id" INTEGER PRIMARY KEY NOT NULL,
-  "name" VARCHAR(45),
-  "version" VARCHAR(45),
-  "os" VARCHAR(45),
-  CONSTRAINT "id_UNIQUE"
-    UNIQUE("id")
+  id INTEGER PRIMARY KEY NOT NULL,
+  name VARCHAR(45),
+  version VARCHAR(45),
+  os VARCHAR(45)
 );
 CREATE TABLE users(
-  "id" INTEGER PRIMARY KEY NOT NULL,
-  "firstName" VARCHAR(45),
-  "lastName" VARCHAR(45),
-  CONSTRAINT "idUsers_UNIQUE"
-    UNIQUE("id")
+  id INTEGER PRIMARY KEY NOT NULL,
+  firstName VARCHAR(45),
+  lastName VARCHAR(45)
 );
 CREATE TABLE websites(
-  "id" INTEGER PRIMARY KEY NOT NULL,
-  "name" VARCHAR(45),
-  "jahia" TEXT,
-  "wordpress" TEXT,
-  "userview" TEXT,
-  "userpwd" TEXT,
-  CONSTRAINT "id_UNIQUE"
-    UNIQUE("id")
+  id INTEGER PRIMARY KEY NOT NULL,
+  name VARCHAR(45),
+  jahia TEXT,
+  wordpress TEXT,
+  userview TEXT,
+  userpwd TEXT
 );
 CREATE TABLE logs(
-  "date" DATETIME NOT NULL,
-  "status" VARCHAR(45),
-  "userId" INTEGER NOT NULL,
-  "browserId" INTEGER NOT NULL,
-  "websiteId" INTEGER NOT NULL,
-  PRIMARY KEY("websiteId","userId","browserId","date"),
-    FOREIGN KEY("browserId")
-    REFERENCES browsers("id")
-    ON UPDATE CASCADE,
-    FOREIGN KEY("websiteId")
-    REFERENCES websites("id")
-    ON UPDATE CASCADE,
-    FOREIGN KEY("userId")
-    REFERENCES users("id")
-    ON UPDATE CASCADE
+  date DATETIME NOT NULL,
+  status VARCHAR(45),
+  userId INTEGER NOT NULL,
+  browserId INTEGER NOT NULL,
+  websiteId INTEGER NOT NULL,
+  PRIMARY KEY(browserId, websiteId, userId)
+ FOREIGN KEY(browserId)
+ REFERENCES browsers(id)
+ ON UPDATE CASCADE,
+ FOREIGN KEY(websiteId)
+ REFERENCES websites(id)
+ ON UPDATE CASCADE,
+ FOREIGN KEY(userId)
+ REFERENCES users(id)
+ ON UPDATE CASCADE
 );
 COMMIT;
