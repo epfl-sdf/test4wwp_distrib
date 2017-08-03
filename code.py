42337d35269044567234f9a02247e95d6b6eeb0c
S-Rey <samuel.rey@epfl.ch>
2017-08-03 13:41:31 +0200

    Fonction de base de la base de données

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ urls = (
 
 db = web.database(dbn='sqlite', db='../credentials/distrib.db')
 names = db.query('SELECT id, first_name, last_name FROM users').list()
-status = ['DONE', 'BUSY', 'EMPTY', 'CONNECTION ERROR', None]
+status = ['DONE', 'STARTED', 'EMPTY', 'CONNECTION ERROR', None]
 
 button = form.Form(
     form.Button("submit", type="submit", description="Next"),
@@ -25,19 +25,59 @@ button = form.Form(
 
 class query:
     @staticmethod
-    def get_urls(user_id, browser_info):
-	urls = db.query(('SELECT s.jahia, s.wordpress '
+    def get_url(user_id, browser_info):
+        # search for 'started' site
+	urls = db.query(('SELECT s.id, s.jahia, s.wordpress, l.status '
                         +'FROM logs l INNER JOIN browsers b ON l.browser_id = b.id '
                         +'INNER JOIN websites s ON l.website_id = s.id '
                         +'WHERE l.user_id = ' + user_id 
                         +' AND b.name = "' + browser_info['browser']['name'] + '"' 
                         +' AND b.os = "' + browser_info['platform']['name'] + '"'
-                        +' AND l.status = "BUSY" '
-                        +'ORDER BY date(date) DESC Limit 1;')).list()
+                        +'ORDER BY l.date DESC LIMIT 1;')).list()
+        # if 'started' site
+        if urls: 
+            if urls[0].status == 'STARTED':
+                return urls[0]
+            else:
+                urls = None
+        # if no 'started' site
         if not urls:
-	    urls = db.query('SELECT jahia, wordpress FROM websites WHERE STATUS IS NULL;').list()
-        return urls
+	    urls = db.query(('SELECT w.id, jahia, wordpress FROM '
+                            + '(logs INNER JOIN websites w ON NOT logs.website_id = w.id INNER JOIN browsers b)'
+                            + ' WHERE NOT (b.name = "' + browser_info['browser']['name'] + '"' 
+                            + ' AND b.os = "' + browser_info['platform']['name'] + '");')).list()
+        # if logs is empty
+        if not urls:
+            urls = db.query('SELECT id, jahia, wordpress FROM websites;').list()
+        rdm_id = randint(0, len(urls)-1)
+        url = urls[rdm_id]
+        url.status = None
+        return url
+    
+    @staticmethod
+    def get_browser_id(browser_info):
+        browser_id = db.query(( 'SELECT id FROM browsers b '
+                        +'WHERE b.name = "' + browser_info['browser']['name'] + '" '
+                        +'AND b.version = "' + browser_info['browser']['version'] + '"'
+                        +'AND b.os = "' + browser_info['platform']['name'] + '";'
+                    )).list()
+        if browser_id:
+            return browser_id[0].id
+        else:
+            return None
 
+    @staticmethod
+    def add_browser(browser_info):
+        return db.insert('browsers', name = browser_info['browser']['name'], 
+                version = browser_info['browser']['version'], os = browser_info['platform']['name'])
+    
+    @staticmethod
+    def add_log(user_id, browser_id, website_id, status):
+        db.insert('logs', user_id = user_id, browser_id = browser_id, website_id = website_id, date = time.time(), status = status)
+
+    @staticmethod
+    def get_id_from_jahia_url(jahia):
+        return db.query('SELECT id FROM websites WHERE jahia = "' + jahia + '";').list()[0].id
 
 class index:
     def GET(self):
@@ -62,17 +102,16 @@ class compare:
         if url1 and url2:
             return render.compare(names, user_id, status, url1, url2)
         else:
-	    browser_info =  httpagentparser.detect(web.ctx.env.get('HTTP_USER_AGENT'))
-            print('--------------------------------\n')
-            print(browser_info)
-            urls = query.get_urls(user_id, browser_info)
-	    if not urls:
+	    browser_info = httpagentparser.detect(web.ctx.env.get('HTTP_USER_AGENT'))
+            browser_id = query.get_browser_id(browser_info)
+            if not browser_id:
+                browser_id = query.add_browser(browser_info)
+            url = query.get_url(user_id, browser_info)
+	    if not url:
                 return "No more sites to compare"
-        rdm_id = randint(0, len(urls)-1)
-        temp = urls[rdm_id]
-        if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
-            db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', USER_ID=user_id, DATE=time.time())
-        raise web.seeother('/compare?user_id=' + user_id + '&url1=' + temp.JAHIA + '&url2=' + temp.WORDPRESS)
+        if not url.status:
+            query.add_log(user_id, browser_id, url.id, 'STARTED')
+        raise web.seeother('/compare?user_id=' + user_id + '&url1=' + url.jahia + '&url2=' + url.wordpress)
 
     def POST(self):
         user_id = web.input(select=None).select
@@ -86,9 +125,18 @@ class compare:
 class next:
     def POST(self):
         statu = web.input(select = None).select
-        print(statu)
         if statu:
-            update_status(statu)
+            browser_info =  httpagentparser.detect(web.ctx.env.get('HTTP_USER_AGENT'))
+            browser_id = query.get_browser_id(browser_info)
+            url = web.input(url1=None).url1
+            user_id = web.input(user_id=None).user_id
+            if user_id != '0':
+                
+                query.add_log(user_id, browser_id, query.get_id_from_jahia_url(url), statu)
+                raise web.seeother('/compare?user_id=' + user_id)
+            else:
+                raise web.seeother('/')
+
         else:
             url1 = web.input(url1=None).url1
             url2 = web.input(url2=None).url2
@@ -98,15 +146,6 @@ class next:
                     raise web.seeother('/')
             if url1 and url2:
                 return render.compare(names, user_id, status, url1, url2)
-
-def update_status(status):
-    url = web.input(url1=None).url1
-    user_id = web.input(user_id=0).user_id
-    if user_id != '0':
-        db.update('sites', where='JAHIA="' + url + '"', STATUS=status, DATE=time.time())
-        raise web.seeother('/compare?user_id=' + user_id)
-    else:
-        raise web.seeother('/')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
