56091db1d84b9f903ea6534147ba32697b3db3e8
streitlua <streitlua@gmail.com>
2017-08-07 14:14:43 +0200

    Formatation de la date dans export_logs.py

--- export_logs.py
+++ export_logs.py
@@ -9,7 +9,8 @@ path = '../credentials/'
 con = sqlite3.connect(path + 'distrib.db')
 cursor = con.cursor()
 data = cursor.execute("SELECT s.id, s.jahia, s.wordpress, b.os, b.name, l.date, u.id, u.first_name, u.last_name, l.status FROM logs l INNER JOIN browsers b ON l.browser_id=b.id INNER JOIN users u ON l.user_id=u.id INNER JOIN websites s ON l.website_id=s.id ORDER BY l.date DESC;")
-
+for elem in data:
+	elem[5] = datetime.datetime.fromtimestamp(str(elem[5]).strftime('%Y-%m-%d_%H:%M:%S'))
 date = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
 csv_path = path + 'logs-' + date + '.csv'
 with open(csv_path, "wb") as csv_file:
