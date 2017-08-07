c2ecd8c422da604c1944833121432320ad781804
streitlua <streitlua@gmail.com>
2017-08-07 12:58:15 +0200

    Modification de export_logs.py pour bien exporter toutes les informations

--- export_logs.py
+++ export_logs.py
@@ -8,14 +8,14 @@ import datetime
 path = '../credentials/'
 con = sqlite3.connect(path + 'distrib.db')
 cursor = con.cursor()
-data = cursor.execute("select * from logs")
+data = cursor.execute("SELECT s.id, s.jahia, s.wordpress, b.os, b.name, l.date, u.id, u.first_name, u.last_name, l.status FROM logs l INNER JOIN browsers b ON l.browser_id=b.id INNER JOIN users u ON l.user_id=u.id INNER JOIN websites s ON l.website_id=s.id ORDER BY l.date DESC;")
 
 date = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
 csv_path = path + 'logs-' + date + '.csv'
 with open(csv_path, "wb") as csv_file:
     csv_writer = csv.writer(csv_file)
     # Write headers.
-    csv_writer.writerow(['user_id', 'browser_id', 'website_id', 'date', 'status'])
+    csv_writer.writerow(['website_id', 'jahia', 'wordpress', 'os', 'browser_name', 'date', 'user_id', 'first_name', 'last_name', 'status'])
     csv_writer.writerows(data)
 #    csv_writer.writerow([cursor])
     # Write data.
