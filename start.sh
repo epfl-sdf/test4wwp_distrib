ae5bdc7f9fad0fc776bb5302b5a0022dc0ead223
S-Rey <samuel.rey@epfl.ch>
2017-08-09 15:32:26 +0200

    Start.sh lance un export_logs.py chaque heure (15h,16h, etc…)+

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+LAST_BACKUP_TIME=-1
+CURR_TIME=$(date +%H)
+
 source venvDistrib/bin/activate 
 python code.py 8081
+
+# Exporte le logs chaques heures
+while :
+do
+	CURR_TIME=$(date +%H)
+	if ! [[ "$LAST_BACKUP_TIME" == "$CURR_TIME" ]];
+	then
+		LAST_BACKUP_TIME=$CURR_TIME
+		python export_logs.py
+	fi
+	sleep 2
+done
